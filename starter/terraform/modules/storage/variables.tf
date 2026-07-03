312ece6dea56e4456fd594242c61ba71cee125b0
malikcherfi <malikcherfi@gmail.com>
2026-07-03 12:10:59 +0200

    refactor variables.tf to include default values for storage module variables

--- starter/terraform/modules/storage/variables.tf
+++ starter/terraform/modules/storage/variables.tf
@@ -1,4 +1,20 @@
-variable "owner" { type = string }
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "tags" { type = map(string) }
+variable "owner" {
+  type    = string
+  default = "malik"
+}
+variable "resource_group_name" {
+  type    = string
+  default = "mcherfiRG"
+}
+variable "location" {
+  type    = string
+  default = "francecentral"
+}
+variable "tags" {
+  type    = map(string)
+  default = "managed_by : cli environment=tp owner=malik-cherf"
+}
+variable "id" {
+  type    = string
+  default = "5e683e0f-b00c-48d6-9769-5aaf598de8f1"
+}
