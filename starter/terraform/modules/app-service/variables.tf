e2273bdd575d16770012014b8329d39e3f4c6683
malikcherfi <malikcherfi@gmail.com>
2026-07-03 15:18:30 +0200

    refactor: expand variable definitions in variables.tf for clarity

--- starter/terraform/modules/app-service/variables.tf
+++ starter/terraform/modules/app-service/variables.tf
@@ -1,4 +1,25 @@
-variable "owner" { type = string }
-variable "resource_group_name" { type = string }
-variable "service_plan_id" { type = string }
-variable "tags" { type = map(string) }
+variable "owner" {
+  type    = string
+  default = "malik"
+}
+variable "resource_group_name" {
+  type    = string
+  default = "mcherfiRG"
+}
+variable "location" {
+  type    = string
+  default = "francecentral"
+}
+variable "tags" {
+  type = map(string)
+  default = {
+    "managed_by"  = "cli"
+    "environment" = "tp"
+    "owner"       = "malik-cherfi"
+  }
+}
+variable "service_plan_id" {
+  type    = string
+  default = "5e683e0f-b00c-48d6-9769-5aaf598de8f1"
+}
+
