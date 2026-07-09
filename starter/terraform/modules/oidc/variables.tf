4e5037332de9d7fc5c95d66fc6eac3c6daf745f2
malikcherfi <malikcherfi@gmail.com>
2026-07-09 14:13:52 +0200

    feat: add OIDC variables for identity management configuration

---
+++ starter/terraform/modules/oidc/variables.tf
@@ -0,0 +1,20 @@
+variable "owner" { type = string }
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "tags" { type = map(string) }
+variable "managed_identity_name" {
+  type    = string
+  default = "github-oidc-identity"
+}
+variable "fic_main_name" {
+  type    = string
+  default = "github-federated-identity-main"
+}
+variable "fic_feat_name" {
+  type    = string
+  default = "github-federated-identity-feat-terraform-config"
+}
+variable "role_definition_name" {
+  type    = string
+  default = "creator-role"
+}
