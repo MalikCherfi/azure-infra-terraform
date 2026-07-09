052be1e47ff2ef66995df97f0a6e423db631484d
malikcherfi <malikcherfi@gmail.com>
2026-07-09 14:38:45 +0200

    fix: remove default values for variables in app-service module

--- starter/terraform/modules/app-service/variables.tf
+++ starter/terraform/modules/app-service/variables.tf
@@ -1,25 +1,16 @@
 variable "owner" {
   type    = string
-  default = "malik"
 }
 variable "resource_group_name" {
   type    = string
-  default = "rg-malik-cherfi"
 }
 variable "location" {
   type    = string
-  default = "francecentral"
 }
 variable "tags" {
   type = map(string)
-  default = {
-    "managed_by"  = "cli"
-    "environment" = "tp"
-    "owner"       = "malik-cherfi"
-  }
 }
 variable "service_plan_id" {
   type    = string
-  default = "5e683e0f-b00c-48d6-9769-5aaf598de8f1"
 }
 
