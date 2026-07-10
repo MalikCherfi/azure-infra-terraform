53a2979e3457ced5b46d537563d1a3e27fa0bc13
malikcherfi <malikcherfi@gmail.com>
2026-07-10 11:03:50 +0200

    feat: consolidate variable definitions in storage module for improved readability

--- starter/terraform/modules/storage/variables.tf
+++ starter/terraform/modules/storage/variables.tf
@@ -1,12 +1,4 @@
-variable "owner" {
-  type    = string
-}
-variable "resource_group_name" {
-  type    = string
-}
-variable "location" {
-  type    = string
-}
-variable "tags" {
-  type = map(string)
-}
+variable "owner" { type = string }
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "tags" { type = map(string) }
