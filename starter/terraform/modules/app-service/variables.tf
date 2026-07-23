59af1ddae9b1b7b425201b9fe1b7f3dc4a9a13b6
malikcherfi <malikcherfi@gmail.com>
2026-07-23 14:07:51 +0200

    feat(variables): add app insights connection string variable

--- starter/terraform/modules/app-service/variables.tf
+++ starter/terraform/modules/app-service/variables.tf
@@ -3,4 +3,8 @@ variable "resource_group_name" { type = string }
 variable "location" { type = string }
 variable "tags" { type = map(string) }
 variable "service_plan_id" { type = string }
+variable "app_insights_connection_string" {
+  type      = string
+  sensitive = true
+}
 
