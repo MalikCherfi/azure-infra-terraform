03826f4d2ec49e8c77d1477301bc010fabc5b7fd
malikcherfi <malikcherfi@gmail.com>
2026-07-27 11:52:22 +0200

    feat(grafana): add Grafana dashboard module with role assignment and workspace integration

---
+++ starter/terraform/modules/grafana/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.9"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+data "azurerm_monitor_workspace" "prometheus" {
+  name                = "amw-malik-cherfi"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_dashboard_grafana" "grafana" {
+  name                  = "grafana-monitoring-${var.owner}"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  location              = data.azurerm_resource_group.rg.location
+  grafana_major_version = "12"
+  tags                  = var.tags
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  azure_monitor_workspace_integrations {
+    resource_id = data.azurerm_monitor_workspace.prometheus.id
+  }
+}
+
+resource "azurerm_role_assignment" "grafana_monitoring_reader" {
+  scope                = data.azurerm_resource_group.rg.id
+  role_definition_name = "Monitoring Reader"
+  principal_id         = azurerm_dashboard_grafana.grafana.identity[0].principal_id
+}
