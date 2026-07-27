39323d7f2456559c3ec52696de532de7daba9d8e
malikcherfi <malikcherfi@gmail.com>
2026-07-27 11:54:12 +0200

    feat(outputs): add Prometheus NIC and public IP outputs

--- starter/terraform/modules/network/outputs.tf
+++ starter/terraform/modules/network/outputs.tf
@@ -9,3 +9,11 @@ output "subnet_frontend_id" {
 output "subnet_backend_id" {
   value = azurerm_subnet.backend.id
 }
+
+output "prometheus_nic_id" {
+  value = azurerm_network_interface.prometheus.id
+}
+
+output "prometheus_public_ip" {
+  value = azurerm_public_ip.prometheus.ip_address
+}
