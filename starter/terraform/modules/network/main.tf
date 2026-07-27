2c450c06ea9dc0cc18ca26bce785f374e8e2e282
malikcherfi <malikcherfi@gmail.com>
2026-07-27 11:54:08 +0200

    feat(network): add Prometheus subnet and associated network security group with rules

--- starter/terraform/modules/network/main.tf
+++ starter/terraform/modules/network/main.tf
@@ -34,9 +34,16 @@ resource "azurerm_subnet" "backend" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_subnet" "prometheus" {
+  name                 = "subnet-prometheus"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
 # Documentation : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group
 
-resource "azurerm_network_security_group" "nsg" {
+resource "azurerm_network_security_group" "nsg-frontend" {
   name                = "nsg-frontend-${var.owner}-tf"
   resource_group_name = var.resource_group_name
   location            = var.location
@@ -80,11 +87,69 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
     description                = "Deny all inbound traffic"
   }
+
 }
 
-# Documentation : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet_network_security_group_association
+resource "azurerm_network_security_group" "nsg-prometheus" {
+  name                = "nsg-prometheus-${var.owner}-tf"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = var.tags
+  security_rule {
+    name                       = "Allow-Prometheus"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9090"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    description                = "Allow Prometheus inbound"
+  }
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    description                = "Allow SSH inbound"
+  }
+}
 
 resource "azurerm_subnet_network_security_group_association" "frontend_nsg" {
   subnet_id                 = azurerm_subnet.frontend.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+  network_security_group_id = azurerm_network_security_group.nsg-frontend.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "prometheus_nsg" {
+  subnet_id                 = azurerm_subnet.prometheus.id
+  network_security_group_id = azurerm_network_security_group.nsg-prometheus.id
+}
+
+resource "azurerm_public_ip" "prometheus" {
+  name                = "pip-prometheus-${var.owner}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
+resource "azurerm_network_interface" "prometheus" {
+  name                = "nic-prometheus-${var.owner}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.prometheus.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.prometheus.id
+  }
 }
