ee8509478ac0205d32e5adaa5886ea20f8ad49c6
malikcherfi <malikcherfi@gmail.com>
2026-07-28 09:33:27 +0200

    fix(grafana): correct reference to Application Insights in scheduled query alert scopes

--- starter/terraform/modules/grafana/main.tf
+++ starter/terraform/modules/grafana/main.tf
@@ -89,7 +89,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "alerte_taux_erreur" {
   location             = var.location
   evaluation_frequency = "PT5M"
   window_duration      = "PT5M"
-  scopes               = [azurerm_application_insights.appi.id]
+  scopes               = [data.azurerm_application_insights.appi.id]
   severity             = 2
 
   criteria {
